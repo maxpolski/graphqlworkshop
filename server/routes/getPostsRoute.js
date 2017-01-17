178786033cb4a546986b1aec6bb687cec970aadb
Max Polski <maxpolski@nixsolutions.com>
2017-01-17 13:02:43 +0200

    added logic for fetching a list of posts

---
+++ server/routes/getPostsRoute.js
@@ -0,0 +1,20 @@
+import Post from '../data/models/post';
+
+export default (req, res) => {
+  const {
+    pageNum,
+  } = req.params;
+  const LIMIT = 10;
+  const query = Post.find({}).skip((pageNum - 1) * LIMIT).limit(LIMIT);
+  query.exec((err, posts) => {
+    if (!err && posts) {
+      return res
+              .set('Content-Type', 'application/json')
+              .send(JSON.stringify(posts));
+    }
+
+    return res
+            .set('Content-Type', 'application/json')
+            .send(JSON.stringify([]));
+  });
+};
